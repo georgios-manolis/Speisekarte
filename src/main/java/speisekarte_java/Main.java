50327e1f90f837c2838f4cda14fedde028c42d52
Georgios Manolis <g-manolis@gmx.de>
2026-03-26 14:30:29 +0100

    Output first line of menue

--- src/main/java/speisekarte_java/Main.java
+++ src/main/java/speisekarte_java/Main.java
@@ -1,7 +1,21 @@
 package speisekarte_java;
 
+import java.io.InputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Programm gestartet");
+    //starte Main
+    public static void main(String[] args) throws Exception{
+        //lädt die Speisekarte aus dem resources Ordner
+        InputStream liesKarte = Main.class
+                .getClassLoader()
+                .getResourceAsStream("data/Speisekarte.txt");
+        //Übersetze eingelesene Daten...
+        BufferedReader uebersetzung = new BufferedReader(new InputStreamReader(liesKarte));
+        String ausgabe = uebersetzung.readLine();
+
+        //...und gib aus, was du übersetzt hast
+        System.out.println(ausgabe);
     }
 }
