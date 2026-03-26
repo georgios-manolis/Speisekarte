00120cd91463faacd8b1dc80d5c204603410a170
Georgios Manolis <g-manolis@gmx.de>
2026-03-26 13:41:49 +0100

    Speisekarte.txt moved and changes in main

--- src/main/java/speisekarte_java/Gericht.java
+++ src/main/java/speisekarte_java/Gericht.java
@@ -1,4 +1,10 @@
 package speisekarte_java;
 
 public class Gericht {
+    int id;
+    String gerichtName;
+    String portionGroesse;
+    double preis;
+    String kategorie;
+    String beschreibung;
 }
