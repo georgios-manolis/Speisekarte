4120ea4aa4d218ce548b957b25a06d40d06c899a
Georgios Manolis <g-manolis@gmx.de>
2026-03-27 13:57:31 +0100

    Implement endless-loop, to readout menue, until no products left

--- src/main/java/menue_java/Main.java
+++ src/main/java/menue_java/Main.java
@@ -7,15 +7,20 @@ import java.io.InputStreamReader;
 public class Main {
     //starte Main
     public static void main(String[] args) throws Exception{
+        String outputMenue = "keine Speisekarte eingelesen";
         //lädt die Speisekarte aus dem resources Ordner
         InputStream readMenue = Main.class
                 .getClassLoader()
                 .getResourceAsStream("data/Speisekarte.txt");
         //Übersetze eingelesene Daten...
         BufferedReader translate = new BufferedReader(new InputStreamReader(readMenue));
-        String outputMenue = translate.readLine();
+        //zeige Kopfzeile ID, Gericht, etc. an
+        outputMenue = translate.readLine();
 
-        //...und gib aus, was du übersetzt hast
-        System.out.println(outputMenue);
+        //lies und übersetze so lange, bis keine zeile mehr da ist
+        while(outputMenue != null) {
+            System.out.println(outputMenue);
+            outputMenue = translate.readLine();
+        }
     }
 }
