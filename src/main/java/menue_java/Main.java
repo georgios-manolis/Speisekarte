d1abbc9ca17a289f0e4c424129cc59105133753d
Georgios Manolis <g-manolis@gmx.de>
2026-03-27 13:33:52 +0100

    Changes in variable naming

--- src/main/java/menue_java/Main.java
+++ src/main/java/menue_java/Main.java
@@ -1,4 +1,4 @@
-package speisekarte_java;
+package menue_java;
 
 import java.io.InputStream;
 import java.io.BufferedReader;
@@ -8,14 +8,14 @@ public class Main {
     //starte Main
     public static void main(String[] args) throws Exception{
         //lädt die Speisekarte aus dem resources Ordner
-        InputStream liesKarte = Main.class
+        InputStream readMenue = Main.class
                 .getClassLoader()
                 .getResourceAsStream("data/Speisekarte.txt");
         //Übersetze eingelesene Daten...
-        BufferedReader uebersetzung = new BufferedReader(new InputStreamReader(liesKarte));
-        String ausgabe = uebersetzung.readLine();
+        BufferedReader translate = new BufferedReader(new InputStreamReader(readMenue));
+        String outputMenue = translate.readLine();
 
         //...und gib aus, was du übersetzt hast
-        System.out.println(ausgabe);
+        System.out.println(outputMenue);
     }
 }
---
+++ src/main/java/menue_java/Product.java
@@ -0,0 +1,10 @@
+package menue_java;
+
+public class Product {
+    int id;
+    String productName;
+    String portion;
+    double price;
+    String category;
+    String description;
+}
