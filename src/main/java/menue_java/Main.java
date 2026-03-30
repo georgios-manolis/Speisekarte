0901aea94e3d73172162c54479ca02478c4e4d43
Georgios Manolis <g-manolis@gmx.de>
2026-03-30 19:21:31 +0200

    corrected variable name

--- src/main/java/menue_java/Main.java
+++ src/main/java/menue_java/Main.java
@@ -7,19 +7,19 @@ import java.io.InputStreamReader;
 public class Main {
     //starte Main
     public static void main(String[] args) throws Exception{
-        String outputMenue = "keine Speisekarte eingelesen";
+        String inputMenue = "keine Speisekarte eingelesen";
         //lädt die Speisekarte aus dem resources Ordner
         InputStream readMenue = Main.class
                 .getClassLoader()
                 .getResourceAsStream("data/Speisekarte.txt");
         //lies die erste Zeile und übersetze die Daten
         BufferedReader translate = new BufferedReader(new InputStreamReader(readMenue));
-        outputMenue = translate.readLine();
+        inputMenue = translate.readLine();
 
         //lies und übersetze so lange, bis keine Zeile mehr da ist
-        while(outputMenue != null) {
-            System.out.println(outputMenue);
-            outputMenue = translate.readLine();
+        while(inputMenue != null) {
+            System.out.println(inputMenue);
+            inputMenue = translate.readLine();
         }
     }
 }
